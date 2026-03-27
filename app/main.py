f358e1f41144985d2c11a76f699e99071c5527f6
ShadowAdi <shadowshukla76@gmail.com>
2026-03-27 18:13:05 +0530

    added global handler

--- app/main.py
+++ app/main.py
@@ -1,6 +1,18 @@
 from fastapi import FastAPI
 from .db import Base,engine
+from .core.expectations import (
+    http_exception_handler,
+    validation_exception_handler,
+    global_exception_handler
+)
+from fastapi.exceptions import RequestValidationError
+from starlette.exceptions import HTTPException as StarletteHTTPException
+
 app = FastAPI()
+
+app.add_exception_handler(StarletteHTTPException, http_exception_handler)
+app.add_exception_handler(RequestValidationError, validation_exception_handler)
+app.add_exception_handler(Exception, global_exception_handler)
 
 Base.metadata.create_all(bind=engine)
 
