641fa4db761c027480baa0914949465d170407f7
ShadowAdi <shadowshukla76@gmail.com>
2026-03-28 01:19:55 +0530

    added register service

---
+++ app/service/user_service.py
@@ -0,0 +1,38 @@
+from sqlalchemy.orm import Session
+from fastapi import HTTPException, status
+from app.models.User import User
+from schemas.user import RegisterRequest, LoginRequest
+from auth.utils import hash_password, verify_password, create_access_token
+from app.core.logger import logger
+
+class UserService:
+    def register(self,db:Session,data:RegisterRequest)->dict:
+        existing=db.query(User).filter(User.email==data.email).first()
+        if existing:
+            logger.error('Email already registered')
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="Email already registered"
+            )
+        
+        if len(data.password) < 4:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="Password must be at least 4 characters"
+            )
+        
+        user = User(
+            email         = data.email,
+            password_hash = hash_password(data.password),
+            role          = data.role,
+            business_name = data.business_name,
+            city          = data.city,
+            phone         = data.phone,
+        )
+        
+        db.add(user)
+        db.commit()
+        db.refresh(user)
+        
+        return user
+        
