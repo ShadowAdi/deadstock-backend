e7117b8a2350395eeee5ab07da3f3685710ad8c8
ShadowAdi <shadowshukla76@gmail.com>
2026-03-28 01:26:50 +0530

    added login

--- app/service/user_service.py
+++ app/service/user_service.py
@@ -36,3 +36,21 @@ class UserService:
         
         return user
         
+    
+    def login(self, db: Session, data: LoginRequest) -> dict:
+        user=db.query(User).filter(User.email==data.email).first()
+        
+        if not user or not verify_password(data.password, user.password_hash):
+            logger.error('Invalid email or password')
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Invalid email or password"
+            )
+        
+        token = create_access_token(user_id=str(user.id), role=user.role)
+
+        return {
+            "access_token": token,
+            "token_type":   "bearer",
+            "user":         user
+        }
