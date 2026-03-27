56cd343b84513c0eec8b1d09c3e2b852b96b3b55
ShadowAdi <shadowshukla76@gmail.com>
2026-03-28 02:30:34 +0530

    added analtyitics route

---
+++ app/routes/analytics.py
@@ -0,0 +1,39 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from app.db import get_db
+from app.auth.dependencies import require_seller, get_current_user
+from service.analytics_service import analytics_service
+from schemas.base import BaseResponse
+from models.User import User
+
+router = APIRouter(prefix="/analytics", tags=["Analytics"])
+
+@router.get("/trending", response_model=BaseResponse)
+def get_trending(db: Session = Depends(get_db)):
+    data = analytics_service.get_trending_categories(db)
+    return BaseResponse(
+        success = True,
+        message = "Trending categories fetched",
+        data    = data
+    )
+    
+@router.get("/savings", response_model=BaseResponse)
+def get_savings(db: Session = Depends(get_db)):
+    data = analytics_service.get_total_savings(db)
+    return BaseResponse(
+        success = True,
+        message = "Platform savings fetched",
+        data    = data
+    )
+    
+@router.get("/dashboard", response_model=BaseResponse)
+def get_dashboard(
+    seller: User    = Depends(require_seller),
+    db:     Session = Depends(get_db)
+):
+    data = analytics_service.get_seller_dashboard(db, seller)
+    return BaseResponse(
+        success = True,
+        message = "Dashboard data fetched",
+        data    = data
+    )
