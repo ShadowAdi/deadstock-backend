a28fd3a6a3764175af5e813242cc4823ebde9531
ShadowAdi <shadowshukla76@gmail.com>
2026-03-28 11:36:24 +0530

    fix base issues

--- app/schemas/base.py
+++ app/schemas/base.py
@@ -4,7 +4,7 @@ from typing import Any, Optional
 class BaseResponse(BaseModel):
     success: bool
     message: str
-    data: Optional[any]=None
+    data: Optional[Any]=None
     
 class ErrorResponse(BaseResponse):
     success: bool = False
